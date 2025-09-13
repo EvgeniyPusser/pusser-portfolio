fff90cb01fce9f05f2a2ad56d277fdb9a9d7d463
Evgeniy Pusser <36860378+EvgeniyPusser@users.noreply.github.com>
2025-09-14 00:35:19 +0300

    Headers fixed to new config

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,11 @@ export default function Header() {
       <h1>Evgenii Bogomazov-Pusser</h1>
       <nav>
         <Link to="/">Home</Link> |<Link to="/about">About</Link> |
-        <Link to="/projects">Projects</Link> |
         <Link to="/education">Education</Link> |
-        <Link to="/support">Support</Link>
+        <Link to="/support">Support</Link> |
+        <Link to="/developer">Developer</Link> |
+        <Link to="/blockchain">Blockchain</Link> |
+        <Link to="/reconstruction">Reconstruction</Link>
       </nav>
     </header>
   );
